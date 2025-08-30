14933b766ce72255890821bbfbbe6f2df2c605f5
johnson Nicholas <nicholasjohnson.njt@gmail.com>
2025-08-31 00:18:14 +0100

    mobile nav

---
+++ index.js
@@ -0,0 +1,23 @@
+// dropdown
+const dropdown = document.getElementById('dropdown');
+const dropdownChild = document.getElementById('dropdown-cild');
+
+dropdown.addEventListener('click', () => {
+    dropdownChild.classList.toggle('hidden');
+});
+
+const menu = document.getElementById('menu');
+const cancel = document.getElementById('cancel');
+const nav = document.getElementById('navigation');
+
+menu.addEventListener('click', () => {
+    nav.classList.contains('-translate-x-[30em]');
+    nav.classList.remove('-translate-x-[30em]');
+    nav.classList.add('translate-x-0');
+});
+
+cancel.addEventListener('click', () => {
+    nav.classList.remove('translate-x-0');
+    nav.classList.add('-translate-x-[30em]');
+});
+
