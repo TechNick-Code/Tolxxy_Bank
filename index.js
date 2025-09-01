b112d7530c6ece368488516d2c7941b67eebdd6e
johnson Nicholas <nicholasjohnson.njt@gmail.com>
2025-09-01 22:15:23 +0100

    mobile navigation

--- index.js
+++ index.js
@@ -5,33 +5,18 @@ const dropDown3 = document.getElementById('dropDown3');
 const dropDownChild = document.getElementById('dropDown-child');
 const dropDownChild2 = document.getElementById('dropDown-child2');
 const dropDownChild3 = document.getElementById('dropDown-child3');
+   
 
-document.addEventListener('click', (event) => {
-    // if (dropDownChild.classList.contains('hidden')) {
-    //     dropDownChild.classList.remove('hidden');
-    // }
-    if (dropDownChild &&
-        !dropDown.contains(event.target) &&
-        !dropDownChild.contains(event.target))
-    dropDownChild.classList.add('hidden');
-    if (dropDownChild2 &&
-        !dropDown2.contains(event.target) &&
-        !dropDownChild2.contains(event.target))
-    dropDownChild2.classList.add('hidden');
-    if (dropDownChild3 &&
-        !dropDown3.contains(event.target) &&
-        !dropDownChild3.contains(event.target))
-    dropDownChild3.classList.add('hidden');
+dropDown.addEventListener('click', () => {
+    dropDownChild.classList.toggle('hidden');
+});
+dropDown2.addEventListener('click', () => {
+    dropDownChild2.classList.toggle('hidden');
 });
 
-// dropDown2.addEventListener('click', () => {
-//     dropDownChild2.classList.toggle('hidden');
-// });
-
-// dropDown3.addEventListener('click', () => {
-//     dropDownChild3.classList.toggle('hidden');
-// });
-
+dropDown3.addEventListener('click', () => {
+    dropDownChild3.classList.toggle('hidden');
+});
 
 
 
